aa1f892378841806d4d081171e7f286f9cdb7d93
Knirta <myknirtak@gmail.com>
2021-08-08 18:10:38 +0300

    step 1 completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 class App extends Component {
   state = {
@@ -6,15 +7,61 @@ class App extends Component {
     name: "",
   };
 
+  handleChange = (e) => {
+    this.setState({ name: e.currentTarget.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.addContact();
+    this.reset();
+  };
+
+  addContact = () => {
+    const contact = {
+      id: uuidv4(),
+      name: this.state.name,
+    };
+
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  reset = () => {
+    this.setState({ name: "" });
+  };
+
   render() {
     return (
-      <input
-        type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-        required
-      />
+      <>
+        <section>
+          <h1>Phonebook</h1>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+                required
+                onChange={this.handleChange}
+                value={this.state.name}
+              />
+            </label>
+            <button type="submit">Add contact</button>
+          </form>
+        </section>
+        <section>
+          <h2>Contacts</h2>
+          <ul>
+            {this.state.contacts.map((contact) => (
+              <li key={contact.id}>{contact.name}</li>
+            ))}
+          </ul>
+        </section>
+      </>
     );
   }
 }
